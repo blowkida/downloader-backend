f06ab9013619a0dbbd59092aaefad66b643a7007
Blow Kida <kathaloknishant@gmail.com>
2025-07-25 00:48:35 +0530

    Fix: use process.env.PORT for Render

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const puppeteer = require("puppeteer");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 app.use(cors());
 app.use(express.json());
 
